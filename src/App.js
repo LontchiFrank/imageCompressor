b6d9e5163495e0ad869a39f67ddd0edeac0d7952
LontchiFrank <fodjof6@gmail.com>
2022-04-07 19:45:47 +0100

    so we thank God

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FormInput from './pages/FormInput.page';
+import Home from './pages/Home.page';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home />
+       */}
+       <FormInput/>
+    </div>
+  );
+}
+
+export default App;
